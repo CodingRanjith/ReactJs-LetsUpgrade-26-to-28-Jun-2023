2e72e5e7f0905fb25d0a173d1ca9fc7de61fe7e4
C.RANJITH KUMAR <83603627+CodingRanjith@users.noreply.github.com>
2023-06-27 19:48:15 +0530

    Create Day 2 Props, State, and Basic Hooks React.js

---
+++ Day 2 Props, State, and Basic Hooks React.js
@@ -0,0 +1,93 @@
+Day 2
+ 
+Props, State, and Basic Hooks React.js
+
+Src folder is important to write react codes like App function.
+
+App Function run the very important for index.js folder.
+
+App.css Folder used to write the css codes.
+
+jsx
+
+javascript xml
+
+It is 99.99% similar to Javascrift.
+The extension we use for jsx is .jsx
+
+To create a react code to create components function.
+
+create components folder for scr folder.
+  componets inside to create react files.
+  example 
+
+comp1.jsx
+comp2.jsx
+
+To add the componets files inside the App.js file.
+
+rfce Command
+
+rfce is automatically create a function.
+
+To create componets only used for file name of first letter Capital letter otherwise not working or reflexting the componets .
+
+To import the componets in App.js 
+
+To App.js file open 
+
+import Comp1 from './file=location';
+
+To creating 100 of componets files to link to App.js function It work the 100 of componets codes.
+
+
+index.css and index.js is a single file your hole react application.  
+
+
+Ways to add css in react.
+
+1.inline css
+
+example
+     <h1 style=""></h1>
+
+2.internal css 
+
+example
+
+  <head>
+<style>
+
+  .heaader{
+}
+</style.css
+  </head>
+
+3.external css
+
+example 
+
+    To write file css code
+
+    style.css file to create inside to write css codes
+
+ After write head tag 
+
+  <head>
+  <link href="/.style.css" rel="stylesheet"/>
+  </head>
+
+
+
+
+
+  
+  
+  
+  
+  
+  
+
+
+  
+  
